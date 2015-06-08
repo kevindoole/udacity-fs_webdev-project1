68620309bde25b7c6181d2b6cdfadb7877a25882
Kevin Doole <kdoole@farmmedia.com>
2015-06-08 18:15:10 -0500

    fix: Use the right name for title

--- movies.py
+++ movies.py
@@ -1,9 +1,11 @@
-"""Provides data about my most favouritest movies"""
+"""Displays a page that shows my most favouritest movies"""
+
+import fresh_tomatoes
 
 class Movie(object):
     """Contains information needed to present a movie"""
 
-    def __init__(self, movie_title, poster_image_url, trailer_youtube_url):
-        self.movie_title = movie_title
+    def __init__(self, title, poster_image_url, trailer_youtube_url):
+        self.title = title
         self.poster_image_url = poster_image_url
         self.trailer_youtube_url = trailer_youtube_url
