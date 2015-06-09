6d0362b9b7467be24f4d4c827a404d0711d2c866
Kevin Doole <kdoole@farmmedia.com>
2015-06-08 21:49:37 -0500

    refactor: Pull js into file

--- fresh_tomatoes.py
+++ fresh_tomatoes.py
@@ -53,31 +53,6 @@ main_page_head = '''
             background-color: white;
         }
     </style>
-    <script type="text/javascript" charset="utf-8">
-        // Pause the video when the modal is closed
-        $(document).on('click', '.hanging-close, .modal-backdrop, .modal', function (event) {
-            // Remove the src so the player itself gets removed, as this is the only
-            // reliable way to ensure the video stops playing in IE
-            $("#trailer-video-container").empty();
-        });
-        // Start playing the video whenever the trailer modal is opened
-        $(document).on('click', '.movie-tile', function (event) {
-            var trailerYouTubeId = $(this).attr('data-trailer-youtube-id')
-            var sourceUrl = 'http://www.youtube.com/embed/' + trailerYouTubeId + '?autoplay=1&html5=1';
-            $("#trailer-video-container").empty().append($("<iframe></iframe>", {
-              'id': 'trailer-video',
-              'type': 'text-html',
-              'src': sourceUrl,
-              'frameborder': 0
-            }));
-        });
-        // Animate in the movies when the page loads
-        $(document).ready(function () {
-          $('.movie-tile').hide().first().show("fast", function showNext() {
-            $(this).next("div").show("fast", showNext);
-          });
-        });
-    </script>
 </head>
 '''
 
@@ -112,6 +87,7 @@ main_page_content = '''
     <div class="container">
       {movie_tiles}
     </div>
+    <script type="text/javascript" charset="utf-8" src="resources/js.js"></script>
   </body>
 </html>
 '''
