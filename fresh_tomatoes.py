6d6544523ea34483eeaceafddfb655d941984ce8
Kevin Doole <kdoole@farmmedia.com>
2015-06-09 00:18:42 -0500

    feat: Support retina devices

--- fresh_tomatoes.py
+++ fresh_tomatoes.py
@@ -102,7 +102,7 @@ main_page_content = '''
 # A single movie entry html template
 movie_tile_content = '''
 <div class="col-md-6 col-lg-4 movie-tile text-center" data-trailer-youtube-id="{trailer_youtube_id}" data-toggle="modal" data-target="#trailer">
-    <img src="{poster_image_url}" width="220" height="342">
+    <img srcset="{srcset}" src="{poster_image_url}" width="220" height="342">
     <h2>{movie_title}</h2>
     <p class="more-info"></p>
 </div>
@@ -116,7 +116,8 @@ def create_movie_tiles_content(movies):
         content += movie_tile_content.format(
             movie_title=movie.title,
             poster_image_url=movie.poster_image_url,
-            trailer_youtube_id=movie.youtube_id()
+            trailer_youtube_id=movie.youtube_id(),
+            srcset = movie.srcset()
         )
     return content
 
--- movies.py
+++ movies.py
@@ -8,7 +8,7 @@ class Movie(object):
 
     def __init__(self, title, poster_image_url, trailer_youtube_url):
         self.title = title
-        self.poster_image_url = poster_image_url
+        self.poster_image_url = 'resources/posters/' + poster_image_url
         self.trailer_youtube_url = trailer_youtube_url
 
     def youtube_id(self):
@@ -18,20 +18,27 @@ class Movie(object):
         trailer_youtube_id = youtube_id_match.group(0) if youtube_id_match else None
         return trailer_youtube_id
 
+    def srcset(self):
+        """Creates an appropriate srcset for serving retina images"""
+        file_extension_start = self.poster_image_url.find('.jpg')
+        srcset_1x = self.poster_image_url + ' 1x'
+        srcset_2x = self.poster_image_url[:file_extension_start] + '@2x.jpg 2x'
+        return srcset_1x + ', ' + srcset_2x
+
 movies = [
     Movie(
         'Life of Brian',
-        'http://ayay.co.uk/movies/poster/comedy/life-of-brian-re-release-2004.jpg',
+        'life-of-brian.jpg',
         'https://www.youtube.com/watch?v=tOOVm4kY4lE'
     ),
     Movie(
         'The Meaning of Life',
-        'http://cdn.collider.com/wp-content/uploads/monty-python-the-meaning-of-life-poster.jpg',
+        'meaning-of-life.jpg',
         'https://www.youtube.com/watch?v=54ulXNk-bmo'
     ),
     Movie(
         'Monty Python and The Holy Grail',
-        'http://lsc.mit.edu/schedule/2014.2q/poster-montypythonandtheholygrail.jpg',
+        'holy-grail.jpg',
         'https://www.youtube.com/watch?v=LG1PlkURjxE'
     )
 ]
